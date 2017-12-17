6406bede0a34983024a369b4d81ab8533d6cb9ae
Kris <karma18@gmail.com>
2017-12-17 13:28:04 -0500

    Updated hw3.py - added list item (amended user)

--- hw3.py
+++ hw3.py
@@ -1,6 +1,6 @@
 # Sample program - Hello World (for loop, build-in enumerate function, new style pythonic formatting!
 
-tags=['World', 'Earch', 'Mars', 'Sun', 'Milky Way']
+tags=['World', 'Earch', 'Mars', 'Jupiter', 'Sun', 'Milky Way']
 
 for i, tag in enumerate(tags):
     print ("Hello {name} \t [iteration: {iter}]".format(iter=i, name=tag))
